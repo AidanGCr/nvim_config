1f1bf24e0fa02d48bfb93eecc9a771099cee2331
Aidan Cruickshank <aidangcruickshank@gmail.com>
2024-09-01 19:06:36 -0400

    Added VimTex plugin and some lsp servers.

--- init.lua
+++ init.lua
@@ -437,6 +437,17 @@ require('lazy').setup({
     end,
   },
 
+  -- Installing VimTex here.
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release/
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
